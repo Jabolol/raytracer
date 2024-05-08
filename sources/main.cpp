1b5bc0682b7077116b4acceaf1e45f18fe686f76
Javier R <javier.rios-urbano@epitech.eu>
2024-05-08 21:07:31 +0200

    add: `render` logic

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,9 +1,4 @@
-#include "arguments/Materials.hpp"
-#include "arguments/Shapes.hpp"
-#include "config/Factory.hpp"
 #include "config/Manager.hpp"
-#include "core/Camera.hpp"
-#include "core/Scene.hpp"
 
 int main(int argc, char **argv)
 {
@@ -11,11 +6,16 @@ int main(int argc, char **argv)
 
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <config file>" << std::endl;
-        return 1;
+        return 84;
     }
 
-    if (!manager.parse(argv[1])) {
-        return 1;
+    try {
+        manager.parse(argv[1]);
+        manager.bootstrap();
+        manager.render();
+    } catch (const Raytracer::Exceptions::Base &e) {
+        std::cerr << e.what() << std::endl;
+        return 84;
     }
 
     return 0;
