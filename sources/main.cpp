12a8141e4ba496440703a5e7fd310594094917f1
Javier R <javier.rios-urbano@epitech.eu>
2024-05-09 03:32:46 +0200

    add: support for `--fast` flag

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,15 +4,21 @@ int main(int argc, char **argv)
 {
     Raytracer::Config::Manager manager;
 
-    if (argc != 2) {
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <config file>" << std::endl;
         return 84;
     }
 
+    std::string flag = "--fast";
+    bool fast = std::find(argv, argv + argc, flag) != argv + argc;
+    auto path = std::find_if(argv + 1, argv + argc, [](const char *arg) {
+        return arg[0] != '-';
+    });
+
     try {
-        manager.parse(argv[1]);
+        manager.parse(*path);
         manager.bootstrap();
-        manager.render();
+        manager.render(fast);
     } catch (const Raytracer::Exceptions::Base &e) {
         std::cerr << e.what() << std::endl;
         return 84;
